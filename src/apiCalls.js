2dcecb3e69e42f894f580254aed836afd3c610e2
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-21 17:52:51 -0400

    Add failsafe for api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,6 +9,7 @@ export const getRandomDeck = (index) => {
 }
 
 export const getChosenDeck = (str) => {
+  if (str === '') str = 'amount=10'
   return fetch('https://opentdb.com/api.php?' + str)
   .then(res => res.json())
 }
