46f98359d18feec204aed40005362a562e0d9ba7
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-21 08:39:56 -0400

    Build initial Join Game component

---
+++ src/components/JoinGameForm/JoinGameForm.js
@@ -0,0 +1,10 @@
+import './JoinGame.scss'
+export const JoinGameForm = () => {
+  return (
+    <div>Join Game
+      <input placeholder="Room Number"/>
+      <input placeholder="Username"/>
+      <button>Join</button>
+    </div>
+  )
+}
