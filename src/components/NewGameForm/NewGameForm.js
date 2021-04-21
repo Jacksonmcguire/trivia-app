e9b4069c0da9c64515bb74366e6f044c677bf117
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-21 14:00:02 -0400

    Get form functionality working

--- src/components/NewGameForm/NewGameForm.js
+++ src/components/NewGameForm/NewGameForm.js
@@ -3,31 +3,65 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export class NewGameForm extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      difficulty: '',
+      category: '',
+      amount: '',
+      type: '',
+      room: '',
+    }
+  }
+
   categoryOptions = () => {
     return this.props.categories.map((category, index) => {
-      console.log(category)
-      return <option key={index}>{category.name}</option>
+      return <option key={index} value={category.id}>{category.name}</option>
     })
+  }
+
+  handleChange = (e) => {
+    if (e.target.name !== 'type') {
+      this.setState({...this.state, [e.target.name]: e.target.value.toLowerCase()})
+    } else this.setState({...this.state, [e.target.name]: e.target.id})
+  } 
+
+  fetchTriviaSet = () => {
+    let fetchUrl = '';
+    Object.keys(this.state).forEach((prop, index) => {
+      if (this.state[prop] !== '' && index !== 0 && prop !== 'room') {
+        fetchUrl += `&${prop + '=' + this.state[prop]}`
+      }
+      else if (this.state[prop] !== '' && index === 0) {
+        fetchUrl = `${prop + '=' + this.state[prop]}`
+      }
+    })
+    this.props.generateSlideDeck(fetchUrl)
   }
 
   render () {
     return (
-    <form className="new-game">New Game
-      <select>
+    <form className="new-game"
+    onChange={(e) => this.handleChange(e)}>New Game
+      <select name="difficulty">
         <option>Difficulty</option>
         <option>Easy</option>
         <option>Medium</option>
         <option>Hard</option>
       </select>
-      <select>
+      <select name="category">
         <option>Category</option>
         {this.categoryOptions()}
       </select>
-      <input type="number" placeholder="Number of Slides"/>
-      <input type="radio" name="question-type"/>
-      <input type="radio" name="question-type"/>
-      <input placeholder="Room Name"/>
-      <Link to="/play">
+      <input type="number" name="amount" placeholder="Number of Slides"/>
+      <div>
+      <input type="radio" name="type" id="boolean"/>
+      <label htmlFor="boolean">True / False</label>
+      <input type="radio" name="type" id="multiple"/>
+      <label htmlFor="multiple">Multiple Choice</label>
+      </div>
+      <input name="room" placeholder="Room Name"/>
+      <Link to="/play" onClick={() => this.fetchTriviaSet()}>
       <button>Start</button>
       </Link>
     </form>
