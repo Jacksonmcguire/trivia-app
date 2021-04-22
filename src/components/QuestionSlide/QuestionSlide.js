29c5639054b675fba2cd749f3d9c3b6b9dbad83a
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-21 21:31:49 -0400

    Update questionSlide to use checkboxes and style

--- src/components/QuestionSlide/QuestionSlide.js
+++ src/components/QuestionSlide/QuestionSlide.js
@@ -1,5 +1,7 @@
 import './QuestionSlide.scss'
 import { useState } from 'react'
+import { decodeHTML } from '../../utilities'
+
 export const QuestionSlide = ({category, incorrectAnswers, correct, question, type, evaluateAnswer}) => {
 
   const [answer, setAnswer] = useState('')
@@ -11,8 +13,8 @@ export const QuestionSlide = ({category, incorrectAnswers, correct, question, ty
     }
     return incorrectAnswers.map((answer, index) => {
       return <div className="option">
-      <input type="radio" id={'opt' + index + 1} name="option"/>
-      <label htmlFor={'opt' + index + 1}>{answer}</label>
+      <input type="checkbox" id={'opt' + index + 1} name="option"/>
+      <label htmlFor={'opt' + index + 1}>{decodeHTML(answer)}</label>
       </div>
     })
     
@@ -20,22 +22,26 @@ export const QuestionSlide = ({category, incorrectAnswers, correct, question, ty
 
   const answerQuestion = (e) => {
     e.preventDefault()
-    if (answer !== '') evaluateAnswer(correct, answer)
+    if (answer !== '') evaluateAnswer(decodeHTML(correct), answer)
     clearInputs()
   }
 
   const updateAnswer = (e) => {
+    const inputs = document.querySelectorAll('input[type="checkbox"]')
+    inputs.forEach(input => {
+      if (input !== e.target) input.checked = false;
+    })
     setAnswer(e.target.labels[0].innerText)
   }
 
   const clearInputs = () => {
-    const inputs = document.querySelectorAll('input[type="radio"]')
+    const inputs = document.querySelectorAll('input[type="checkbox"]')
     inputs.forEach(input => input.checked = false)
   } 
 
   return (
     <form className="current-q" onChange={(e) => updateAnswer(e)}>
-      <h3>{question}</h3>
+      <h3>{decodeHTML(question)}</h3>
       {addAnswers()}
       <button onClick={answerQuestion}>Submit Answer</button>
     </form>
