50a5decaceef3d81d80dd9c4d11055982c964c10
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-21 21:27:17 -0400

    Add a go back link to end slide

--- src/components/EndSlide/EndSlide.js
+++ src/components/EndSlide/EndSlide.js
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
 import './EndSlide.scss';
 export const EndSlide = ({slides, score}) => {
   return (
     <form className="current-q">
       <h3>You Scored {score + '/' + slides.length}</h3>
+      <Link to="/">Back to Lobby</Link>
     </form>
   )
 }
