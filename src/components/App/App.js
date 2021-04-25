4a700e5db98bb83ae5aa58a703b494c3b930e06b
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-25 18:23:01 -0400

    Begin working with localstorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,27 +12,47 @@ class App extends React.Component {
     this.state = {
       activeSlides: [],
       activeSearch: '',
-      gameScore: {correct: 0, total: 0, incorrect: [], correctQuestions: []}
+      gameScore: {correct: 0, total: 0, incorrect: [], correctQuestions: []},
+      totalStats: []
     }
+  }
+
+  componentDidMount() {
+    const scores = JSON.parse(localStorage.getItem('scores'))
+    this.setState({...this.state, totalStats: (scores || [])})
+    console.log((scores))
   }
 
   generateSlideDeck = (str) => {
     getChosenDeck(str)
     .then(data => {
-      console.log(data)
       this.setState({activeSlides: data.results, activeSearch: str})
     })
   }  
 
   startNewRound = ({correct, length, incorrect, correctAnswers}) => {
+    this.endRound(correct, length, incorrect, correctAnswers)
     this.generateSlideDeck(this.state.activeSearch)
-    
+    console.log(JSON.parse(localStorage.getItem('scores')))
+  }
+
+  endRound = (correct, length, incorrect, correctAnswers) => {
     this.setState({...this.state, gameScore: {
       correct: this.state.gameScore.correct + correct,
       total: this.state.gameScore.total + length, 
       incorrect: [...this.state.gameScore.incorrect, ...incorrect],
       correctQuestions: [...this.state.gameScore.correctQuestions, ...correctAnswers]
-    }})
+  }
+  })
+  console.log(this.state.gameScore, correct, length, incorrect, correctAnswers)
+  localStorage.setItem('scores', JSON.stringify([...this.state.totalStats, this.state.gameScore]))
+
+  }
+
+  endGame = ({correct, length, incorrect, correctAnswers}) => {
+    console.log(correct, length, incorrect, correctAnswers)
+    this.endRound(correct, length, incorrect, correctAnswers)
+
   }
 
   render() {
@@ -40,12 +60,15 @@ class App extends React.Component {
       <div className="App">
         <header>Trivia App</header>
         <Switch>
-        <Route exact path='/'><Lobby generateSlideDeck={this.generateSlideDeck}/></Route>
+        <Route exact path='/'>
+          <Lobby generateSlideDeck={this.generateSlideDeck}/>
+        </Route>
         <Route exact path='/play'>
           <InGame 
           slides={this.state.activeSlides} 
           startNewRound={this.startNewRound}
           gameStats={this.state.gameScore}
+          endGame={this.endGame}
           />
         </Route>
         </Switch>
