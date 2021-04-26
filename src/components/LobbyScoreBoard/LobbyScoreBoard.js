55de03ec55c7e891b89583e20db24f87629fa040
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-26 16:51:42 -0400

    Implement clear history and fix proptypes + minor styling

--- src/components/LobbyScoreBoard/LobbyScoreBoard.js
+++ src/components/LobbyScoreBoard/LobbyScoreBoard.js
@@ -1,10 +1,13 @@
 import { Popover } from "@headlessui/react"
 import { ChevronDownIcon } from "@heroicons/react/solid"
-import { array, object } from "prop-types"
+import { array, func } from "prop-types"
+import { Link } from "react-router-dom"
 import { decodeHTML } from "../../utilities"
 import './LobbyScoreBoard.scss'
 
-export const LobbyScoreBoard = ({totalStats}) => {
+export const LobbyScoreBoard = ({totalStats, clearHistory}) => {
+
+
   const previousGames = totalStats.map((game, index) => {
     const incorrectAnswers = game.incorrect.map((incorrect, index) => <p key={index}>{decodeHTML(incorrect.question)}<br></br> You answered: {decodeHTML(incorrect.answer)}</p>)
     const correctAnswers = game.correctQuestions.map((correct) => <p key={correct}>{decodeHTML(correct)}</p>)
@@ -28,6 +31,9 @@ export const LobbyScoreBoard = ({totalStats}) => {
 
   return (
     <div className="lobby-score">
+      <Link to='/'>
+      <button onClick={clearHistory}>Clear History</button>
+      </Link>
       <h2>Previous Games:</h2>
       {previousGames}
     </div>
@@ -35,5 +41,6 @@ export const LobbyScoreBoard = ({totalStats}) => {
 }
 
 LobbyScoreBoard.propTypes = {
-  totalStats: array,
+  totalStats: array.isRequired,
+  clearHistory: func.isRequired
 }
