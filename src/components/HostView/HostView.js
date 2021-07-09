fb8d236a9a0a09ba98efac2bc68de70e6034c101
Jackson McGuire <jaxmcguire@gmail.com>
2021-07-09 08:05:18 -0400

    Add basic hostview styling

--- src/components/HostView/HostView.js
+++ src/components/HostView/HostView.js
@@ -2,34 +2,42 @@ import { decodeHTML } from '../../utilities'
 import { BiBadgeCheck, BiX } from 'react-icons/bi'
 import './HostView.scss'
 export const HostView = ({room, slideDeck, players}) => {
-  const questions = <ul>{slideDeck && slideDeck.map(slide => {
+  const questions = <ul className="questions"><h3>Questions</h3>
+  {slideDeck && slideDeck.map((slide, index) => {
     return <li>
+      <p>{index + 1}</p>
       <p>{decodeHTML(slide.question)}</p>
-      <i>{decodeHTML(slide.correct_answer)}</i>
+      <p>{decodeHTML(slide.correct_answer)}</p>
     </li>
   })}</ul>
 
-  const playerScores = <section>
+  const playerScores = <section className="players"><h3>Players</h3>
     {players && players.map(player => {
       return <article className="player-card">
         <h4>{player.name}</h4>
-        <div className="player-score">
-        {player.correct}
-        <BiBadgeCheck></BiBadgeCheck>
-        </div>
-        <div className="player-score">
-        {player.incorrect}
-        <BiX></BiX>
-        </div>
+        <section className="player-scores">
+          <div className="player-score">
+          {player.correct}
+          <BiBadgeCheck></BiBadgeCheck>
+          </div>
+          <div className="player-score">
+          {player.incorrect}
+          <BiX></BiX>
+          </div>
+        </section>
       </article>
     })}
   </section>
   return (
     <section className="host">
-      <h3>Questions</h3>
-      {questions}
-      <h3>Players</h3>
+      <div>
+
+        {questions}
+      </div>
+      <div>Players
+
       {playerScores}
+      </div>
     </section>
   )
 }
